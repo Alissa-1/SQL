4af915ef76481a4fd705a9d829f0e0bd739ca619
Alissa <alena.cheremukhina@gmail.com>
2023-01-24 16:16:17 +0300

    Initial commit

---
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -0,0 +1,32 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+import ru.netology.data.DataHelper;
+
+import java.sql.Connection;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static java.awt.SystemColor.info;
+
+public class LoginPage {
+    private SelenideElement login = $("[data-test-id=login] input");
+    private SelenideElement password = $("[data-test-id=password] input");
+    private SelenideElement loginButton = $("[data-test-id=action-login]");
+    private SelenideElement error = $("[data-test-id=error-notification]");
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        login.setValue(info.getLogin());
+        password.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+    }
+
+    public void getError() {
+        error.shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+}
---
+++ src/test/java/ru/netology/test/BankLoginTest.java
@@ -0,0 +1,46 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BankLoginTest {
+
+    @AfterAll
+    static void clearUp() {
+        SQLHelper.clearDatabase();
+    }
+
+    @Test
+    void shouldSuccessfullyLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+
+    }
+
+    @Test
+    void shouldGetErrorWhenUserDoesNotExistsInBase() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = new DataHelper.AuthInfo(DataHelper.getRandomUser().getLogin(), DataHelper.
+                getAuthInfoWithTestData().getPassword());
+        loginPage.validLogin(authInfo);
+        loginPage.getError();
+    }
+
+    @Test
+    void shouldGerErrorWhenPasswordIsWrong() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = new DataHelper.AuthInfo(DataHelper.getAuthInfoWithTestData().getLogin(), DataHelper.
+                getRandomUser().getPassword());
+        loginPage.validLogin(authInfo);
+        loginPage.getError();
+    }
+
+}
